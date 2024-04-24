b75ffdeef4165b1a5a99645a3c7fb3107f943940
AntonyChR <antonychr99@gmail.com>
2024-04-24 14:59:00 -0500

    add comment

--- internal/domain/title_info_provider.go
+++ internal/domain/title_info_provider.go
@@ -3,5 +3,6 @@ package domain
 import "github.com/AntonyChR/orus-media-server/internal/domain/models"
 
 type TitleInfoProvider interface {
+	// Search for a title info in the provider
 	Search(fileName string) (models.TitleInfo, error)
 }
