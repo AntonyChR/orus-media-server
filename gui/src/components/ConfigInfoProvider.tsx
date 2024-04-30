85fc9446394a000a39d2746ef961c7ae4ee0ce2a
AntonyChR <antonychr99@gmail.com>
2024-04-29 21:23:41 -0500

    Update UI styles in ConfigInfoProvider and VideosWithNoInfo components, and refactor VideosWithNoInfo component to fetch and display video files with no information

--- gui/src/components/ConfigInfoProvider.tsx
+++ gui/src/components/ConfigInfoProvider.tsx
@@ -8,16 +8,33 @@ const ConfigInfoProvider: FC<ConfigInfoProviderProps> = ({ className }) => {
         <div className={`${className}`}>
             <h2 className='text-white text-xl'>Info provider</h2>
             <p className='text-white'>
-                Select the info provider you want to use to get information about the videos
+                Select the info provider you want to use to get information
+                about the videos
             </p>
             <form action='' className=''>
                 <select className='mr-2 my-2' id='selectInfoProvider'>
-                    <option value='imdb'>IMDB</option>
-                    <option value='tmdb'>TMDB</option>
                     <option value='omdb'>OMDB</option>
+                    <option disabled value='imdb'>IMDB</option>
+                    <option disabled value='tmdb'>TMDB</option>
                 </select>
                 <input className='' type='text' placeholder='api key' />
-                <button className='text-white block bg-red-700 p-1 rounded-md my-2 active:bg-red-400' type='submit'>Save</button>
+                <p className='text-white italic text-sm'>
+                    <span className='text-red-500'>* </span>You can get an API key from{' '}
+                    <a
+                        className='text-blue-500'
+                        target='_blank'
+                        href='https://www.omdbapi.com/apikey.aspx'
+                    >
+                        https://www.omdbapi.com/apikey.aspx
+                    </a>
+                </p>
+                <button
+
+                    className='text-white block bg-red-700 p-1 rounded-md my-2 active:bg-red-400'
+                    type='submit'
+                >
+                    Save
+                </button>
             </form>
         </div>
     );
--- gui/src/components/VideosWithNoInfo.tsx
+++ gui/src/components/VideosWithNoInfo.tsx
@@ -5,7 +5,7 @@ interface VideosWithNoInfoProps {
     className?: string;
 }
 
-const VideosWithNoInfo: FC<VideosWithNoInfoProps> = ({className}) => {
+const VideosWithNoInfo: FC<VideosWithNoInfoProps> = ({ className }) => {
     const [videosWithNoInfo, setVideos] = useState<Video[]>([]);
     useEffect(() => {
         ApiDb.getVideoWithNoTitleInfo().then((videos) => {
@@ -16,18 +16,31 @@ const VideosWithNoInfo: FC<VideosWithNoInfoProps> = ({className}) => {
     }, []);
     return (
         <div className={`${className}   `}>
-            <h2 className='text-white text-xl'>Video files with no information</h2>
-            <div className='text-white p-3'>
-            <ul className=''>
-                {videosWithNoInfo.map((video) => {
-                    return (
-                        <li key={video.ID} className='hover:bg-gray-700 hover:cursor-pointer'>
-                            {video.Name}
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+            <h2 className='text-white text-xl'>
+                Video files with no information
+            </h2>
+            {videosWithNoInfo.length === 0 ? (
+                <p className='text-white italic text-sm'>
+                    Here you will see the videos that have no information
+                </p>
+            ) : (
+                <>
+                    <div className='text-white p-3'>
+                        <ul className=''>
+                            {videosWithNoInfo.map((video) => {
+                                return (
+                                    <li
+                                        key={video.ID}
+                                        className='hover:bg-gray-700 hover:cursor-pointer'
+                                    >
+                                        {video.Name}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
