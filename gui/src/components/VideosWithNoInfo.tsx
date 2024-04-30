dcf653181b3d7326eaeb642a8c7040be5a6dbe65
AntonyChR <antonychr99@gmail.com>
2024-04-29 19:01:19 -0500

    Refactor VideosWithNoInfo component to fetch and display video files with no information

---
+++ gui/src/components/VideosWithNoInfo.tsx
@@ -0,0 +1,35 @@
+import { FC, useEffect, useState } from 'react';
+import ApiDb from '../data_fetching/data_fetching';
+import { Video } from '../types/Video';
+interface VideosWithNoInfoProps {
+    className?: string;
+}
+
+const VideosWithNoInfo: FC<VideosWithNoInfoProps> = ({className}) => {
+    const [videosWithNoInfo, setVideos] = useState<Video[]>([]);
+    useEffect(() => {
+        ApiDb.getVideoWithNoTitleInfo().then((videos) => {
+            if (videos) {
+                setVideos(videos);
+            }
+        });
+    }, []);
+    return (
+        <div className={`${className}   `}>
+            <h2 className='text-white text-xl'>Video files with no information</h2>
+            <div className='text-white p-3'>
+            <ul className=''>
+                {videosWithNoInfo.map((video) => {
+                    return (
+                        <li key={video.ID} className='hover:bg-gray-700 hover:cursor-pointer'>
+                            {video.Name}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+        </div>
+    );
+};
+
+export default VideosWithNoInfo;
