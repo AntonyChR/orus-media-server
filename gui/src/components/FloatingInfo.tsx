7f056f3d3c75eaa339c6496ac739068c110ed31d
AntonyChR <antonychr99@gmail.com>
2024-05-02 00:38:20 -0500

    Update FloatingInfo component with fadeIn animation

--- gui/src/components/FloatingInfo.tsx
+++ gui/src/components/FloatingInfo.tsx
@@ -9,7 +9,7 @@ interface FloatingInfoProps {
 const FloatingInfo: FC<FloatingInfoProps> = ({ titleInfo }) => {
     return (
         <div
-            className='text-white absolute top-9 right-[-20%] w-[250px] h-[350px] z-10 p-3 space-y-2 rounded-md bg-gray-950 border border-gray-900 animate-fade'
+            className='text-white absolute top-9 right-[-20%] w-[250px] h-[350px] z-10 p-3 space-y-2 rounded-md bg-gray-950 border border-gray-900 animate-fadeIn'
         >
             <p>{titleInfo.Plot}</p>
             <p>
